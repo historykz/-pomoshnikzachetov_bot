b8f9fd69d3f006b744561e808f6669f6723f9287
historykz <chatgptalina24@gmail.com>
2026-04-13 19:50:17 +0500

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
-ADMIN_IDS_RAW = os.getenv("ADMIN_IDS", "123456789")
+ADMIN_IDS_RAW = os.getenv("ADMIN_IDS", "")
 ADMIN_IDS = [int(x.strip()) for x in ADMIN_IDS_RAW.split(",") if x.strip()]
 
 DB_PATH = os.getenv("DB_PATH", "exam_bot.db")
